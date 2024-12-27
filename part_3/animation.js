6339ee98450ec6381fba60546ea78e0baca1429f
IlKorshunov <145204077+IlKorshunov@users.noreply.github.com>
2024-12-28 02:44:42 +0300

    add part 3 and part 1 for phone

---
+++ part_3/animation.js
@@ -0,0 +1,410 @@
+const first = {
+    title: "Продукты",
+    descriptionItems: [
+        {
+            title: "Вычисления и хранения",
+            id: "second_menu_radio_1",
+            mainItems: [
+                {
+                    title: "Виртуальная инфраструктура",
+                    link: "https://mws.ru/services/virtual-infrastructure/",
+                    subtitle: "Для проектов разных масштабов",
+                    description: "Комплексное решение для развертывания виртуальной ИТ-инфраструктуры",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/VI_icon_5892a15699.svg",
+                },
+                {
+                    title: "Виртуальная инфраструктура. Аттестованный сегмент 152-ФЗ",
+                    link: "https://mws.ru/services/iaas-152-fz/",
+                    subtitle: "",
+                    description: "Современные решения для бизнеса",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/VI_152_icon_fd8ffd9674.svg",
+                },
+                {
+                    title: "Виртуальная инфраструктура с GPU",
+                    link: "https://mws.ru/services/object-storage-s3/",
+                    subtitle: "Графический ускоритель",
+                    description: "",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/s3_icon_0a3a26b5d4.svg",
+                },
+                {
+                    title: "Объектное хранилище",
+                    link: "https://mws.ru/services/object-storage-s3/",
+                    subtitle: "",
+                    description: "Объектное хранилище — сервис надёжного облачного хранения данных разного объёма и формата. Обеспечивает быстрый доступ и раздачу контента через интернет",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/VI_152_icon_fd8ffd9674.svg",
+                },
+                {
+                    title: "Резервное копирование",
+                    link: "https://mws.ru/services/backup/",
+                    subtitle: "",
+                    description: "Резервное копирование данных с ПК, VDI и любых устройств с пользовательскими ОС",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/backup_icon_b427ce391e.svg",
+                },
+                {
+                    title: "VDI",
+                    link: "https://mws.ru/services/vdi-desktops/",
+                    subtitle: "",
+                    description: "Виртуальные рабочие места",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/vdi_icon_73cf48b578.svg",
+                },
+                {
+                    title: "Катастрофоустойчивая инфраструктура",
+                    link: "https://mws.ru/services/disaster-recovery/",
+                    subtitle: "",
+                    description: "Послеаварийное восстановление",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/DR_icon_efd7415ba0.svg",
+                },
+                {
+                    title: "Managed Kubernetes Service",
+                    link: "https://mws.ru/services/managed-kubernetes/",
+                    subtitle: "",
+                    description: "Сервис для быстрого развертывания кластеров Kubernetes",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/Kuber_icon_39ed7a8fdf.svg",
+                },
+                {
+                    title: "Импортозамещённое облако",
+                    link: "https://mws.ru/state-cloud/",
+                    subtitle: "",
+                    description: "Облако с защитой информации. Бета-тест",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/state_cloud_icon_aa739087b6.svg",
+                }
+            ]
+        },
+        {
+            title: "Бизнес-приложения",
+            id: "second_menu_radio_2",
+            mainItems: [
+                {
+                    title: "Офисные приложения",
+                    link: "https://mws.ru/services/business-applications/",
+                    subtitle: "",
+                    description: "сервисы для построения экосистемы цифровых рабочих мест",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/business_apps_icon_814e8b14cc.svg",
+                },
+                {
+                    title: "КЭДО",
+                    link: "https://mws.ru/services/kedo/",
+                    subtitle: "",
+                    description: "Система кадрового электронного документооборота",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/kedo_icon_28f45b2d7f.svg",
+                },
+                {
+                    title: "VDI",
+                    link: "https://mws.ru/services/vdi-desktops/",
+                    subtitle: "",
+                    description: "Виртуальные рабочие места",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/vdi_icon_73cf48b578.svg",
+                }
+            ]
+        }
+    ]
+};
+const second = {
+    title: "Профессиональные сервисы",
+    descriptionItems: [
+        {
+            title: "Консалтинг и проектные услуги",
+            id: "second_menu_radio_12",
+            mainItems: [
+                {
+                    title: "Аудит",
+                    subtitle: "",
+                    description: "анализ",
+                    link: "https://mws.ru/services/it-infrastructure-audit/",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/vdi_icon_73cf48b578.svg",
+                }
+            ]
+        },
+        {
+            title: "Профессиональные сервисы и аутсорсинг",
+            id: "second_menu_radio_22",
+            mainItems: [
+                {
+                    title: "Профессиональные сервисы",
+                    subtitle: "",
+                    description: "Реализация облачных проектов",
+                    link: "https://mws.ru/services/it-infrastructure-audit/",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/vdi_icon_73cf48b578.svg",
+                }
+            ]
+        }
+    ]
+}
+const third = {
+    title: "Партнёрские сервисы",
+    link: "https://mws.ru/partners/partnerskie_resheniya/?utm_source=click_menu"
+};
+const four = {
+    title: "Стать партнёром",
+    link: "https://mws.ru/partners/"
+};
+const five = {
+    title: "Акции",
+    link: "https://mws.ru/promo/"
+};
+const six = {
+    title: "События",
+    descriptionItems: [
+        {
+            title: "Будущие мероприятия",
+            id: "second_menu_radio_16",
+            mainItems: [
+                {
+                    title: "Вебинар по Kubernetes",
+                    subtitle: "",
+                    description: "Обсуждение трендов",
+                    link: "https://mws.ru/events/kubernetes-webinar/",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/state_cloud_icon_aa739087b6.svg",
+                }
+            ]
+        }
+    ]
+};
+const seven = {
+    title: "Поддержка",
+    descriptionItems: [
+        {
+            title: "Техническая поддержка",
+            id: "second_menu_radio_17",
+            mainItems: [
+                {
+                    title: "Чат поддержки",
+                    subtitle: "",
+                    description: "Получите помощь онлайн",
+                    image: "https://ru-msk-2.store.cloud.mts.ru/uploads/state_cloud_icon_aa739087b6.svg",
+                }
+            ]
+        }
+    ]
+};
+
+const eight = {
+    title: "Карьера",
+    link: "https://mws.ru/vacancies/"
+};
+
+const menuMapping = {
+    "first": first,
+    "second": second,
+    "third": third,
+    "four": four,
+    "five": five,
+    "six": six,
+    "seven": seven,
+    "eight": eight,
+};
+
+//  ---------------
+function updateMainMenuSndMenu(mainMenu, mainTitleText) {
+    const mainList = document.querySelector(".grid_table");
+    const mainTitle = document.querySelector(".second_menu_main_title");
+
+    mainList.innerHTML = "";
+    mainMenu.forEach((item) => {
+        const li = document.createElement("li");
+        li.className = "grid_item";
+        li.innerHTML = `
+          <a href="${item.link}" class="link">
+            <img
+              width="40"
+              height="40"
+              srcset="${item.image} 1x, ${item.image} 2x"
+              src="${item.image}"
+            />
+            <div class="grid_item_cell">
+              <span class="grid_item_title">${item.title}</span>
+              <span class="grid_item_subtitle">${item.subtitle}</span>
+              <span class="grid_item_description">${item.description}</span>
+            </div>
+          </a>
+        `;
+        mainList.appendChild(li);
+    });
+    mainList.addEventListener("mouseover", (action) => {
+        const gridItems = action.target.closest(".grid_item");
+        gridItems.style.backgroundColor = "rgba(128, 128, 128, 0.2)";
+    })
+
+    mainList.addEventListener("mouseout", (action) => {
+        const gridItems = action.target.closest(".grid_item");
+        gridItems.style.backgroundColor = "white";
+    })
+    
+    if (mainTitleText) {
+        mainTitle.textContent = mainTitleText;
+    }
+}
+
+function updateLeftItemsSndMenu(number_menu, part_menu = 0) {
+    const menuData = menuMapping[number_menu];
+    if (isNaN(part_menu)) {
+        part_menu = 0;
+    }
+    const submenuTitle = document.querySelector(".second_menu_submenu_title");
+    submenuTitle.textContent = menuData.title;
+
+    const descriptionList = document.querySelector(".white_2nd_menu_description");
+    descriptionList.innerHTML = "";
+    menuData.descriptionItems.forEach((item, index) => {
+        const li = document.createElement("li");
+        li.className = "white_2nd_menu_description_item";
+        li.setAttribute("data_part_menu", index);
+        li.innerHTML = `
+            <input
+                type="radio"
+                id="${item.id}"
+                name="second_menu"
+                class="white_2nd_menu_description_item_button"
+            />
+            <label for="${item.id}" class="second_menu_label">${item.title}</label>
+        `;
+        descriptionList.appendChild(li);
+    });
+
+    setupDescriptionButtons(menuData); 
+    document.querySelector(".white_2nd_menu").classList.add('active');
+}
+
+function setupDescriptionButtons(menuData) {
+    const descriptionList = document.querySelector(".white_2nd_menu_description");
+    const descriptionButtons = document.querySelectorAll(".white_2nd_menu_description_item");
+    const descriptionButtonStates = new Map();
+
+    descriptionButtons.forEach((button) => {
+        descriptionButtonStates.set(button, false);
+        resetButtonStyle(button);
+    });
+
+    const firstButton = descriptionButtons[0];
+    if (firstButton) {
+        descriptionButtonStates.set(firstButton, true);
+        highlightButton(firstButton);
+        updateMainMenuSndMenu(menuData.descriptionItems[0].mainItems, menuData.descriptionItems[0].title);
+    }
+
+    descriptionList.addEventListener("click", (action) => {
+        const button = action.target.closest(".white_2nd_menu_description_item");
+
+        descriptionButtons.forEach((curButton) => {
+            descriptionButtonStates.set(curButton, false);
+            resetButtonStyle(curButton);
+        });
+
+        descriptionButtonStates.set(button, true);
+        highlightButton(button);
+
+        const partMenu = parseInt(button.getAttribute("data_part_menu"), 10);
+        const selectedMenu = menuData.descriptionItems[partMenu];
+        updateMainMenuSndMenu(selectedMenu.mainItems, selectedMenu.title);
+    });
+
+    descriptionList.addEventListener("mouseover", (action) => {
+        const button = action.target.closest(".white_2nd_menu_description_item");
+        button.style.backgroundColor = "rgba(128, 128, 128, 0.2)";
+    });
+
+    descriptionList.addEventListener("mouseout", (action) => {
+        const button = action.target.closest(".white_2nd_menu_description_item");
+        button.style.backgroundColor = "white";
+    });
+}
+
+//  ----------------
+
+
+function resetButtonStyle(button) {
+    const label = button.querySelector('label');
+    label.style.backgroundColor = '';
+    label.style.color = 'black';
+}
+
+function highlightButton(button) {
+    const label = button.querySelector('label');
+    label.style.backgroundColor = 'black';
+    label.style.color = 'white';
+}
+
+
+
+const buttons = document.querySelectorAll('.center_menu .mybutton');
+const buttons_main_menu = document.querySelectorAll('.main_menu_item');
+
+buttons.forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.05)';
+    });
+
+    button.addEventListener('mouseleave', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
+
+buttons_main_menu.forEach(curButton => {
+    curButton.addEventListener('mouseover', () => {
+        curButton.style.color = 'red';
+    });
+
+    curButton.addEventListener('mouseleave', () => {
+        curButton.style.color = '';
+    });
+});
+
+
+
+const mainButtons = document.querySelectorAll('.main_menu_item');
+const buttonStates = new Map();
+
+mainButtons.forEach(button => {
+    buttonStates.set(button, false);
+});
+
+mainButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault(); // у меня изначально происходит дабл-клик при нажатии на label. А здесь я убиваю label и работает только input
+
+        const menuId = button.id;
+        const whiteMenu = document.querySelector('.white_2nd_menu');
+        const partMenu = parseInt(button.getAttribute('data_part_menu'), 10);
+
+        if (menuMapping[menuId].link) {
+            window.location.href = menuMapping[menuId].link; 
+            return;
+        }
+        updateLeftItemsSndMenu(menuId, partMenu);
+
+        mainButtons.forEach(curButton => {
+            if (button != curButton) {
+                const label = curButton.querySelector('.menu_label');
+                const arrow = curButton.querySelector('.menu_arrow');
+
+                if (label) label.style.color = '';
+                if (arrow) {
+                    arrow.style.color = '';
+                    arrow.classList.remove('rotated');
+                }
+                buttonStates.set(curButton, false);
+            }
+        });
+
+        const label = button.querySelector('.menu_label');
+        const arrow = button.querySelector('.menu_arrow');
+        const isActive = buttonStates.get(button);
+
+        if (!isActive) {
+            if (label) label.style.color = 'red';
+            if (arrow) {
+                arrow.style.color = 'red';
+                arrow.classList.add('rotated');
+            }
+            buttonStates.set(button, true);
+        } else {
+            whiteMenu.classList.remove('active');
+            if (label) label.style.color = '';
+            if (arrow) {
+                arrow.style.color = '';
+                arrow.classList.remove('rotated');
+            }
+            buttonStates.set(button, false);
+        }
+    });
+});
